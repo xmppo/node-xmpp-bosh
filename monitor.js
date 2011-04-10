7fd2e8449b7ded301b754ef06002e03c550f3258
Dhruv Matani <dhruvbird@gmail.com>
2011-04-10 10:29:17 +0000

    Added monitor.js.
    Usage: Just run it as $> node monitor.js
    and it will JSLINT files as they are modified and saved.
    
    Note: Requires the node-jslint module to be installed.

---
+++ monitor.js
@@ -0,0 +1,45 @@
+var fs    = require('fs');
+var path  = require('path');
+var cp    = require('child_process');
+var dutil = require('./src/dutil.js');
+
+function get_files(dir) {
+	return fs.readdirSync(dir).map(function(fn) {
+		return path.join(dir, fn);
+	});
+}
+
+var src_files   = get_files('src');
+var tests_files = get_files('src');
+
+
+var files = src_files.concat(tests_files);
+files = files.filter(function(fn) {
+	return fn.search(/.js$/) != -1;
+});
+
+var mtimes = { };
+
+files.forEach(function(fp) {
+	console.log("Watching file:", fp);
+	mtimes[fp] = 0;
+
+	fs.watchFile(fp, function(curr, prev) {
+		// console.log(curr.mtime.getTime(), prev.mtime.getTime());
+		var cmtime = curr.mtime.getTime(), pmtime = prev.mtime.getTime();
+		if (curr.mtime == prev.mtime || mtimes[fp] == cmtime) {
+			return;
+		}
+		mtimes[fp] = cmtime;
+
+		var jslint = cp.spawn('jslint', [ fp ]);
+		var title = "Linting file: " + fp;
+		var underline = dutil.repeat('-', title.length).join('');
+
+		console.log("\n" + title);
+		console.log(underline);
+		jslint.stdout.on('data', function(d) {
+			console.log(d.toString());
+		});
+	});
+});
