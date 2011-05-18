f0a24fbb78c9dd14ee64804e95aed3e4f52fa50c
Dhruv Matani <dhruvbird@gmail.com>
2011-05-18 16:10:57 +0000

    stress.js: * Refactor * Supports 2 request types (delayed and continuous)

--- tests/stress.js
+++ tests/stress.js
@@ -49,96 +49,141 @@ function http_request(options, cb) {
     });
 
     r.on('error', function() {
-	cb(true);
+		cb(true);
     });
 
     r.end(body);
+	return r;
 }
+
+function xml_http_request(options, cb) {
+	return http_request(options, function(err, response) {
+		if (!err) {
+			response = dutil.xml_parse(response);
+		}
+		cb(err, response);
+	});
+}
+
 
 function do_test(domain, route) {
     var rid = 5292811;
     var sid = null;
 
     var sess_create_attrs = {
-	to: domain, 
-	rid: rid++, 
-	hold: 1, 
-	ver: '1.6', 
-	'xmpp:version': '1.0', 
-	wait: '60', 
-	'xml:lang': 'en', 
-	'xmlns:xmpp': 'urn:xmpp:xbosh', 
-	xmlns: BOSH_XMLNS
+		to: domain, 
+		rid: rid++, 
+		hold: 1, 
+		ver: '1.6', 
+		'xmpp:version': '1.0', 
+		wait: '60', 
+		'xml:lang': 'en', 
+		'xmlns:xmpp': 'urn:xmpp:xbosh', 
+		xmlns: BOSH_XMLNS
     };
 
     if (route) {
-	sess_create_attrs.route = route;
+		sess_create_attrs.route = route;
     }
 
     var sess_create = new ltx.Element('body', sess_create_attrs);
     var u = url.parse(options.endpoint);
 
     var http_request_options = {
-	host: u.hostname, 
-	port: u.port, 
-	path: u.pathname, 
-	method: 'POST', 
-	body: sess_create.toString()
+		host: u.hostname, 
+		port: u.port, 
+		path: u.pathname, 
+		method: 'POST', 
+		body: sess_create.toString()
     };
 
     console.log('Connecting to:', domain);
-    http_request(http_request_options, function(err, response) {
-	if (err) {
-	    console.error('Error in request:', sess_create.toString());
-	    return;
-	}
+    xml_http_request(http_request_options, function(err, sess_create_response) {
+		if (err) {
+			console.error('Error in request:', sess_create.toString());
+			return;
+		}
 
-	var sess_create_response = ltx.parse(response);
-	sid = sess_create_response.attrs.sid;
+		sid = sess_create_response.attrs.sid;
 
-	setInterval(function() {
-	    var ebody = new ltx.Element('body', {
-		rid: rid++, 
-		sid: sid
-	    });
+		function send_periodic_request(rid, delay_sec) {
+			setInterval(function() {
+				var ebody = new ltx.Element('body', {
+					rid: rid++, 
+					sid: sid
+				});
 
-	    console.log("Sending request:", ebody.toString());
-	    http_request_options.body = ebody.toString();
-	    http_request(http_request_options, function() { });
+				console.log("[1] Sending request:", ebody.toString());
+				http_request_options.body = ebody.toString();
+				xml_http_request(http_request_options, function() { });
 
-	}, 30*1000);
+			}, options.reqdelay * 1000);
+		}
+
+		function send_request_on_response(rid) {
+			var ebody = new ltx.Element('body', {
+				rid: rid++, 
+				sid: sid
+			});
+
+			console.log("[2] Sending request:", ebody.toString());
+			http_request_options.body = ebody.toString();
+			xml_http_request(http_request_options, function(err) {
+				if (err) {
+					console.error('Error in response for request:', ebody.toString());
+				}
+				else {
+					send_request_on_response(rid);
+				}
+			});
+		}
+
+		if (options.reqdelay) {
+			send_periodic_request(rid, options.reqdelay);
+		}
+		else {
+			send_request_on_response(rid);
+		}
     });
-
 }
 
 function start_test(options) {
     var u = url.parse(options.endpoint);
     var agent = http.getAgent(u.hostname, u.port);
-    agent.maxSockets = 256;
+    agent.maxSockets = options.nconn;
 
-    for (var i = 0; i < options.nconns; ++i) {
-	do_test(options.domain, options.route);
+    for (var i = 0; i < options.nsess; ++i) {
+		do_test(options.domain, options.route);
     }
 }
 
 
 function main() {
     var opts = require('tav').set({
-	domain: {
-	    note: 'The XMPP server of \'domain\' shall be connected to'
-	}, 
-	endpoint: {
-	    note: 'The BOSH service endpoint (default: http://localhost:5280/http-bind/)', 
-	    value: 'http://localhost:5280/http-bind/'
-	}, 
-	route: {
-	    note: 'The route attribute to use (default: <empty>)', 
-	    value: ''
-	}, 
-	nconns: {
-	    note: 'The number of connections to make (default: 2000)', 
-	    value: 2000
-	}
+		domain: {
+			note: 'The XMPP server of \'domain\' shall be connected to'
+		}, 
+		endpoint: {
+			note: 'The BOSH service endpoint (default: http://localhost:5280/http-bind/)', 
+			value: 'http://localhost:5280/http-bind/'
+		}, 
+		route: {
+			note: 'The route attribute to use (default: <empty>)', 
+			value: ''
+		}, 
+		nsess: {
+			note: 'The number of sessions to open (default: 2000)', 
+			value: 2000
+		}, 
+		nconn: {
+			note: 'The number of connections to make (default: 256)', 
+			value: 256
+		}, 
+		reqdelay: {
+			note: 'The number of second to delay successive requests on a BOSH session ' + 
+				' [0 means send immediately after receiving a response] (default: 30)', 
+			value: 30
+		}
     });
 
     options = opts;
