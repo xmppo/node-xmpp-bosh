f76f26291feae03f744dd55300aa88fc0958f0c3
Satyam Shekhar <satyamshekhar@gmail.com>
2012-02-03 21:54:11 +0000

    Replacing console for people who dont have log4js master.

--- src/log.js
+++ src/log.js
@@ -1,9 +1,9 @@
 var log4js = require("log4js");
-
+/*
 log4js.configure({
     doNotReplaceConsole: true
 });
-
+*/
 var appender = log4js.consoleAppender(log4js.basicLayout);
 log4js.clearAppenders();
 log4js.addAppender(appender);
