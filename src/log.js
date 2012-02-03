52423c209c48bbb51115fd77ccae68e182c0ac90
Satyam Shekhar <satyamshekhar@gmail.com>
2012-02-03 21:49:10 +0000

    Adding log.js - required in all prev commits.

---
+++ src/log.js
@@ -0,0 +1,20 @@
+var log4js = require("log4js");
+
+log4js.configure({
+    doNotReplaceConsole: true
+});
+
+var appender = log4js.consoleAppender(log4js.basicLayout);
+log4js.clearAppenders();
+log4js.addAppender(appender);
+log4js.setGlobalLogLevel("INFO");
+
+module.exports = log4js;
+
+module.exports.setLogOptions = function (opts) {
+    var appender = log4js.fileAppender(opts.log_file_name, undefined, opts.max_log_file_size, opts.number_of_log_files);
+
+    log4js.clearAppenders()
+    log4js.addAppender(appender);
+    log4js.setGlobalLogLevel(opts.logging || "INFO");
+};
