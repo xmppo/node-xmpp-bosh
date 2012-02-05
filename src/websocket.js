0ab6732b2b9cc95ef8a5f6c4ab0622bab4c825c9
Satyam Shekhar <satyamshekhar@gmail.com>
2012-02-05 20:05:26 +0000

    bugfix: undefined being logged in websockets.

--- src/websocket.js
+++ src/websocket.js
@@ -183,7 +183,7 @@ exports.createServer = function(bosh_server) {
 		});
 
 		conn.on('close', function() {
-			log.debug("%s Stream close requested");
+			log.debug("%s Stream close requested", stream_name);
 
 			if (!sn_state.hasOwnProperty(stream_name)) {
 				// Already terminated
