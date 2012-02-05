2bc73d44837cf2180a12aca9a6a9dcbe80c92cdf
Satyam Shekhar <satyamshekhar@gmail.com>
2012-02-05 20:45:03 +0000

    Adding logging options to bosh.conf.example.js

--- bosh.conf.example.js
+++ bosh.conf.example.js
@@ -5,6 +5,14 @@ exports.config = {
 	host: '0.0.0.0', 
 	path: /^\/http-bind(\/+)?$/, 
 	logging: 'INFO', 
+	
+	log_file_name: 'nxb.log',
+
+	// the maximum size (in bytes) for a log file, if not provided then logs won't be rotated.
+	max_log_file_size: 50000000,
+
+	// the number of log files to keep after logSize has been reached
+	number_of_log_files: 10,
 
 	// The maximum number of bytes that the BOSH server will 
 	// "hold" from the client
