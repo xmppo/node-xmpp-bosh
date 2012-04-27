f5b1a9029f442705ba57acf606a5f559f55ee34e
Satyam Shekhar <satyamshekhar@gmail.com>
2012-04-27 14:11:53 +0530

    indentation fixes.

--- src/response.js
+++ src/response.js
@@ -30,29 +30,29 @@ var helper       = require('./helper.js');
 var http_headers = require('./http-headers.js');
 var NULL_FUNC    = dutil.NULL_FUNC;
 
-var path        = require('path');
-var filename    = "[" + path.basename(path.normalize(__filename)) + "]";
-var log         = require('./log.js').getLogger(filename);
+var path         = require('path');
+var filename     = "[" + path.basename(path.normalize(__filename)) + "]";
+var log          = require('./log.js').getLogger(filename);
 
 function BoshResponse(res, request_id, sid, options) {
     this.rid        = request_id;
     this._sid       = sid;
-	this._res		= res;
+    this._res       = res;
     this._options   = options;
 }
 
 BoshResponse.prototype = {
     set_timeout: function (func, wait) {
-		this.timeout = setTimeout(func, wait);
-	},
+        this.timeout = setTimeout(func, wait);
+    },
 
     clear_timeout: function () {
-		clearTimeout(this.timeout);
-	},
+        clearTimeout(this.timeout);
+    },
 
     set_error: function (error_func) {
-		this._res.on('error', error_func);
-	},
+        this._res.on('error', error_func);
+    },
     // Sends a stream termination response on an HTTP response (res) object.
     // This method is generally used to terminate rogue connections.
     send_termination_stanza: function (attrs) {
@@ -61,37 +61,37 @@ BoshResponse.prototype = {
         this.send_response(helper.$terminate(attrs).toString(), true);
     },
 
-	// Allow Cross-Domain access
-	// https://developer.mozilla.org/En/HTTP_access_control
-	send_response: function (msg, no_error_handler) {
-		// To prevent an unhandled exception later
-        
-		// if (!no_error_handler) {
-        //     this.set_error(NULL_FUNC);
-        // }
+    // Allow Cross-Domain access
+    // https://developer.mozilla.org/En/HTTP_access_control
+    send_response: function (msg, no_error_handler) {
+        // To prevent an unhandled exception later
+        if (!no_error_handler) {
+            this.set_error(NULL_FUNC);
+        }
+
         // According to the spec. we need to send a Content-Length header
         this._res.setHeader("Content-Length", Buffer.byteLength(msg, 'utf8'));
-		this._res.writeHead(200, http_headers.POST);
-		this._res.end(msg);
-		log.debug("%s SENT(%s): %s", this._sid, this.rid, msg);
-	},
+        this._res.writeHead(200, http_headers.POST);
+        this._res.end(msg);
+        log.debug("%s SENT(%s): %s", this._sid, this.rid, msg);
+    },
 
-	// If a client closes a connection and a response to that HTTP request
-	// has not yet been sent, then the 'error' event is NOT raised by node.js.
-	// Hence, we need not attach an 'error' event handler yet.
+    // If a client closes a connection and a response to that HTTP request
+    // has not yet been sent, then the 'error' event is NOT raised by node.js.
+    // Hence, we need not attach an 'error' event handler yet.
 
-	// res.socket could be undefined if this request's socket is still in the 
-	// process of sending the previous request's response. Either ways, we 
-	// can be sure that setTimeout and setKeepAlive have already been called 
-	// on this socket.
-	set_socket_options: function (wait) {
-		if (this._res.socket) {
-			// Increasing the timeout of the underlying socket to allow 
-			// wait > 120 sec
-			this._res.socket.setTimeout(wait * 1000 + 10);
-			this._res.socket.setKeepAlive(true, this._options.HTTP_SOCKET_KEEPALIVE);
-		}
-	}
+    // res.socket could be undefined if this request's socket is still in the
+    // process of sending the previous request's response. Either ways, we
+    // can be sure that setTimeout and setKeepAlive have already been called
+    // on this socket.
+    set_socket_options: function (wait) {
+        if (this._res.socket) {
+            // Increasing the timeout of the underlying socket to allow
+            // wait > 120 sec
+            this._res.socket.setTimeout(wait * 1000 + 10);
+            this._res.socket.setKeepAlive(true, this._options.HTTP_SOCKET_KEEPALIVE);
+        }
+    }
 };
 
 function JSONPResponseProxy(req, res) {
