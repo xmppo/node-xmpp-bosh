e4477cadb92c4523bdef85b6eafc04b0cd9c72a4
Dhruv Matani <dhruvbird@gmail.com>
2013-06-09 10:30:44 -0700

    Automated testing using 'npm test'.

---
+++ tests/all.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+function cleanup {
+  echo "Cleaning up NXB daemon process and tests (if any)"
+  CPIDS=$(pgrep -s 0)
+  echo "CPIDS: $CPIDS"
+  kill -TERM $CPIDS
+}
+trap cleanup EXIT
+
+node run-server.js &
+
+sleep 1
+WAIT_SEC=3
+echo -ne "Waiting for BOSH server to start... "
+while [ $WAIT_SEC -gt 0 ]
+do
+    echo -ne "$WAIT_SEC  "
+    WAIT_SEC=$(($WAIT_SEC - 1))
+    sleep 1
+done
+echo "second"
+
+bash tests/test_GET.sh &
+wait $!
+if [ $? -ne 0 ]
+then
+    echo -e "\e[00;31mFAILED: tests/test_GET.sh\e[00m" 1>&2
+    exit 1
+else
+    echo -e "\e[00;32mSUCCESS: tests/test_GET.sh\e[00m" 1>&2
+fi
+
+node tests/basic.js --username="nonxbtest@jappix.com" --password="nonxbtest" &
+wait $!
+if [ $? -eq 0 ]
+then
+    echo -e "\e[00;31mFAILED: tests/basic.js\e[00m" 1>&2
+    exit 1
+else
+    echo -e "\e[00;32mSUCCESS: tests/basic.js\e[00m" 1>&2
+fi
+
+node tests/basic.js --username="nxbtest@jappix.com" --password="nonxbtest" &
+wait $!
+if [ $? -eq 0 ]
+then
+    echo -e "\e[00;31mFAILED: tests/basic.js\e[00m" 1>&2
+    exit 1
+else
+    echo -e "\e[00;32mSUCCESS: tests/basic.js\e[00m" 1>&2
+fi
+
+node tests/basic.js --username="nxbtest@jappix.com" --password="nxbtest" &
+wait $!
+if [ $? -ne 0 ]
+then
+    echo -e "\e[00;31mFAILED: tests/basic.js\e[00m" 1>&2
+    exit 1
+else
+    echo -e "\e[00;32mSUCCESS: tests/basic.js\e[00m" 1>&2
+fi
+
+exit 0
